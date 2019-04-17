5d6cbe3390694cc8726b245277fb00909c4b8ef6
jmgorduez <jmgorduez@gmail.com>
2019-04-17 13:38:13 -0500

    Code to pass test isAPeakAndPlateTime() implemented

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/TransitRegulatoryAgencyUIO.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/TransitRegulatoryAgencyUIO.java
@@ -2,9 +2,12 @@ package ec.com.jmgorduez.PeakAndPlatePredictor.domain;
 
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.ILicensePlateNumber;
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.ITransitRegulatoryAgency;
+import ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import static ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants.*;
 
 public class TransitRegulatoryAgencyUIO implements ITransitRegulatoryAgency {
     @Override
@@ -14,7 +17,8 @@ public class TransitRegulatoryAgencyUIO implements ITransitRegulatoryAgency {
 
     @Override
     public Boolean isAPeakAndPlateTime(LocalTime time) {
-        return null;
+        return time.isAfter(_06_59) && time.isBefore(_09_31)
+                || time.isAfter(_15_59) && time.isBefore(_19_31);
     }
 
     @Override
--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/utils/Constants.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/utils/Constants.java
@@ -1,4 +1,10 @@
 package ec.com.jmgorduez.PeakAndPlatePredictor.utils;
 
+import java.time.LocalTime;
+
 public class Constants {
+    public static final LocalTime _06_59 = LocalTime.parse("06:59");
+    public static final LocalTime _09_31 = LocalTime.parse("09:31");
+    public static final LocalTime _15_59 = LocalTime.parse("15:59");
+    public static final LocalTime _19_31 = LocalTime.parse("19:31");
 }
