03d4c49d44904a9b385191e57dd90233eff25560
jmgorduez <jmgorduez@gmail.com>
2019-04-17 12:13:50 -0500

    Changing initialization of LocalDate constanst

--- src/test/java/ec/com/jmgorduez/PeakAndPlatePredictor/dataGenarator/TestDataGenerator.java
+++ src/test/java/ec/com/jmgorduez/PeakAndPlatePredictor/dataGenarator/TestDataGenerator.java
@@ -8,8 +8,8 @@ import java.time.LocalTime;
 import static org.mockito.ArgumentMatchers.any;
 
 public class TestDataGenerator {
-    public static final LocalDate _15_04_2019 = LocalDate.parse("15/04/2019");
-    public static final LocalDate _16_04_2019 = LocalDate.parse("16/04/2019");
+    public static final LocalDate _15_04_2019 = LocalDate.parse("2019-04-15");
+    public static final LocalDate _16_04_2019 = LocalDate.parse("2019-04-16");
 
     public static final LocalTime _07_00 = LocalTime.parse("07:00");
 }
