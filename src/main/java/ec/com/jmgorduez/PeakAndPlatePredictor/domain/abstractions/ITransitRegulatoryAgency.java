58a42ffc75594a24cc7d6ee3086d5a0fcf9a9459
jmgorduez <jmgorduez@gmail.com>
2019-04-17 11:51:09 -0500

    Change of the name

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/abstractions/ITransitRegulatoryAgency.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/abstractions/ITransitRegulatoryAgency.java
@@ -3,8 +3,8 @@ package ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public interface ITransitRegulator {
-    Boolean isAPeakAndPlateDayOfWeek(LocalDate date);
+public interface ITransitRegulatoryAgency {
+    Boolean isAPeakAndPlateDate(LocalDate date);
     Boolean isAPeakAndPlateTime(LocalTime time);
     ILicensePlateNumber instanceLicensePlateNumber(String value);
 }
