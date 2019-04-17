300d90f1312d33f03ad852479688b50be1c8e83e
jmgorduez <jmgorduez@gmail.com>
2019-04-17 14:14:17 -0500

    Method instanceLicensePlateNumber() implemented

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/TransitRegulatoryAgencyUIO.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/TransitRegulatoryAgencyUIO.java
@@ -2,11 +2,12 @@ package ec.com.jmgorduez.PeakAndPlatePredictor.domain;
 
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.ILicensePlateNumber;
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.ITransitRegulatoryAgency;
-import ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants;
+import ec.com.jmgorduez.PeakAndPlatePredictor.domain.enums.LicensePlateNumberTypeUIO;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import static ec.com.jmgorduez.PeakAndPlatePredictor.domain.enums.LicensePlateNumberTypeUIO.*;
 import static ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants.*;
 
 public class TransitRegulatoryAgencyUIO implements ITransitRegulatoryAgency {
@@ -23,6 +24,26 @@ public class TransitRegulatoryAgencyUIO implements ITransitRegulatoryAgency {
 
     @Override
     public ILicensePlateNumber instanceLicensePlateNumber(String value) {
-        return null;
+        Integer lastNumber = lastCharacter(value);
+        switch (lastNumber) {
+            case 0:
+            case 1:
+                return new LicensePlateNumberUIO(LICENSE_PLATE_NUMBER_CAN_BE_NOT_ON_ROAD_ON_MONDAYS);
+            case 2:
+            case 3:
+                return new LicensePlateNumberUIO(LICENSE_PLATE_NUMBER_CAN_BE_NOT_ON_ROAD_ON_TUESDAYS);
+            case 4:
+            case 5:
+                return new LicensePlateNumberUIO(LICENSE_PLATE_NUMBER_CAN_BE_NOT_ON_ROAD_ON_WEDNESDAYS);
+            case 6:
+            case 7:
+                return new LicensePlateNumberUIO(LICENSE_PLATE_NUMBER_CAN_BE_NOT_ON_ROAD_ON_THURSDAYS);
+            default:
+                return new LicensePlateNumberUIO(LICENSE_PLATE_NUMBER_CAN_BE_NOT_ON_ROAD_ON_FRIDAYS);
+        }
+    }
+
+    private int lastCharacter(String value) {
+        return Character.getNumericValue(value.charAt(value.length() - ONE));
     }
 }
--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/utils/Constants.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/utils/Constants.java
@@ -3,6 +3,17 @@ package ec.com.jmgorduez.PeakAndPlatePredictor.utils;
 import java.time.LocalTime;
 
 public class Constants {
+    public static final Integer ONE = 1;
+    public static final Integer TWO = 2;
+    public static final Integer THREE = 3;
+    public static final Integer FOUR = 4;
+    public static final Integer FIVE = 5;
+    public static final Integer SIX = 6;
+    public static final Integer SEVEN = 7;
+    public static final Integer EIGHT = 8;
+    public static final Integer NINE = 9;
+    public static final Integer ZERO = 0;
+
     public static final LocalTime _06_59 = LocalTime.parse("06:59");
     public static final LocalTime _09_31 = LocalTime.parse("09:31");
     public static final LocalTime _15_59 = LocalTime.parse("15:59");
