9e348434f63c3df929f72f44a6283b9d9eead304
jmgorduez <jmgorduez@gmail.com>
2019-04-18 15:44:37 -0500

    holidays() implemented

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/LocalDateCheckerUIO.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/LocalDateCheckerUIO.java
@@ -3,11 +3,15 @@ package ec.com.jmgorduez.PeakAndPlatePredictor.domain;
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.ILocalDateChecker;
 
 import java.time.LocalDate;
+import java.time.MonthDay;
 import java.time.Year;
 import java.util.List;
+import java.util.stream.Collectors;
 
-import static java.time.DayOfWeek.SATURDAY;
-import static java.time.DayOfWeek.SUNDAY;
+import static ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants.*;
+import static ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants.DECEMBER_25;
+import static java.time.DayOfWeek.*;
+import static java.time.DayOfWeek.FRIDAY;
 
 public class LocalDateCheckerUIO implements ILocalDateChecker {
     @Override
@@ -21,6 +25,42 @@ public class LocalDateCheckerUIO implements ILocalDateChecker {
     }
 
     List<LocalDate> holidays(Year year){
-        return null;
+        return HOLIDAYS_UIO.stream()
+                .map(monthDay -> monthDay.atYear(year.getValue()))
+                .map(date -> adjustHolidayDateAccordingLaw(date))
+                .collect(Collectors.toList());
+    }
+
+    private static LocalDate adjustHolidayDateAccordingLaw(LocalDate date) {
+        if (isNotSwitchableHoliday(date) || isNotNecessaryToSwitchDate(date)) {
+            return date;
+        }
+        if (isNecessaryToSwitchOneDayBefore(date)) {
+            return date.minusDays(ONE);
+        }
+        if (isNecessaryToSwitchOneDayAfter(date)) {
+            return date.plusDays(ONE);
+        }
+        return date.plusDays(numbersOfDaysUntilFriday(date));
+    }
+
+    private static int numbersOfDaysUntilFriday(LocalDate date) {
+        return FRIDAY.getValue() - date.getDayOfWeek().getValue();
+    }
+
+    private static boolean isNecessaryToSwitchOneDayAfter(LocalDate date) {
+        return SUNDAY.equals(date.getDayOfWeek());
+    }
+
+    private static boolean isNecessaryToSwitchOneDayBefore(LocalDate date) {
+        return TUESDAY.equals(date.getDayOfWeek()) || SATURDAY.equals(date.getDayOfWeek());
+    }
+
+    private static boolean isNotNecessaryToSwitchDate(LocalDate dateOfYear) {
+        return dateOfYear.getDayOfWeek().equals(MONDAY) || dateOfYear.getDayOfWeek().equals(FRIDAY);
+    }
+
+    private static boolean isNotSwitchableHoliday(LocalDate date) {
+        return MonthDay.from(date).equals(JANUARY_01) || MonthDay.from(date).equals(DECEMBER_25);
     }
 }
