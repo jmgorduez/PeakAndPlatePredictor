820a609ca174999050733bcd853bc035ae0afc70
jmgorduez <jmgorduez@gmail.com>
2019-04-18 09:39:57 -0500

    Class PeakAndPlateLineSplitterTest declared

---
+++ src/test/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitterTest.java
@@ -0,0 +1,27 @@
+package ec.com.jmgorduez.PeakAndPlatePredictor.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeakAndPlateLineSplitterTest {
+
+    private PeakAndPlateLineSplitter peakAndPlateLineSplitterUnderTest;
+
+    @Test
+    void setUp(){
+        peakAndPlateLineSplitterUnderTest = new PeakAndPlateLineSplitter();
+    }
+
+    @Test
+    void licensePlateNumber() {
+    }
+
+    @Test
+    void date() {
+    }
+
+    @Test
+    void time() {
+    }
+}
