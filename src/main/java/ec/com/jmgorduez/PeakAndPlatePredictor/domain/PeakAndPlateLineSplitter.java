581ca17eeb32cb144190b9ead2616c4b66f4409e
jmgorduez <jmgorduez@gmail.com>
2019-04-18 09:43:24 -0500

    Test licensePlateNumber() implemented

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitter.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitter.java
@@ -7,7 +7,7 @@ import java.time.LocalTime;
 
 public class PeakAndPlateLineSplitter implements IPeakAndPlateLineSplitter {
 
-    public PeakAndPlateLineSplitter(){
+    public PeakAndPlateLineSplitter(String line){
 
     }
 
--- src/test/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitterTest.java
+++ src/test/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitterTest.java
@@ -1,20 +1,26 @@
 package ec.com.jmgorduez.PeakAndPlatePredictor.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static ec.com.jmgorduez.PeakAndPlatePredictor.dataGenarator.TestDataGenerator.PCI_8580;
+import static ec.com.jmgorduez.PeakAndPlatePredictor.dataGenarator.TestDataGenerator.PCI_8580_2019_04_15_07_00;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PeakAndPlateLineSplitterTest {
 
     private PeakAndPlateLineSplitter peakAndPlateLineSplitterUnderTest;
 
-    @Test
-    void setUp(){
-        peakAndPlateLineSplitterUnderTest = new PeakAndPlateLineSplitter();
+    @BeforeEach
+    void setUp() {
+        peakAndPlateLineSplitterUnderTest = new PeakAndPlateLineSplitter(PCI_8580_2019_04_15_07_00);
     }
 
     @Test
     void licensePlateNumber() {
+        assertThat(peakAndPlateLineSplitterUnderTest.licensePlateNumber())
+                .isEqualTo(PCI_8580);
     }
 
     @Test
