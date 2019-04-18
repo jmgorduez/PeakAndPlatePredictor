cd66aa16287ce0cfb5d6c8732d4ddbbdeda42e0d
jmgorduez <jmgorduez@gmail.com>
2019-04-18 09:47:02 -0500

    licensePlateNumber() implemented

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitter.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/PeakAndPlateLineSplitter.java
@@ -1,19 +1,28 @@
 package ec.com.jmgorduez.PeakAndPlatePredictor.domain;
 
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.IPeakAndPlateLineSplitter;
+import ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+import static ec.com.jmgorduez.PeakAndPlatePredictor.utils.Constants.BLANK_SPACE_STRING;
 
 public class PeakAndPlateLineSplitter implements IPeakAndPlateLineSplitter {
 
-    public PeakAndPlateLineSplitter(String line){
+    private String licensePlateNumber;
 
+    public PeakAndPlateLineSplitter(String line){
+        Queue<String> lineElements = new ArrayDeque<>(Arrays.asList(line.split(BLANK_SPACE_STRING)));
+        licensePlateNumber = lineElements.poll();
     }
 
     @Override
     public String licensePlateNumber() {
-        return null;
+        return licensePlateNumber;
     }
 
     @Override
