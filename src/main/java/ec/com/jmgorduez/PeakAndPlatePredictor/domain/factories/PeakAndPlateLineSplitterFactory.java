af4cf31ff4f9769b6c577e3d0425ccb3dd96df51
jmgorduez <jmgorduez@gmail.com>
2019-04-19 16:11:29 -0500

    Method instanceSplitter() implemented

--- src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/factories/PeakAndPlateLineSplitterFactory.java
+++ src/main/java/ec/com/jmgorduez/PeakAndPlatePredictor/domain/factories/PeakAndPlateLineSplitterFactory.java
@@ -2,10 +2,14 @@ package ec.com.jmgorduez.PeakAndPlatePredictor.domain.factories;
 
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.factories.IPeakAndPlateSplitterFactory;
 import ec.com.jmgorduez.PeakAndPlatePredictor.domain.abstractions.utils.IPeakAndPlateLineSplitter;
+import ec.com.jmgorduez.PeakAndPlatePredictor.utils.PeakAndPlateLineSplitter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class PeakAndPlateLineSplitterFactory implements IPeakAndPlateSplitterFactory {
     @Override
     public IPeakAndPlateLineSplitter instanceSplitter(String value) {
-        return null;
+        return new PeakAndPlateLineSplitter(value, LocalDate::parse, LocalTime::parse);
     }
 }
